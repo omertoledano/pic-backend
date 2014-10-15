5171dd0e44ec475184dfb8618f6e8112568a2138
Omer Toledano <omertoledano1@gmail.com>
2014-10-15 13:04:14 +0300

    enabled cors

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from eve import Eve
 from flask import jsonify
+from flask_cors import CORS
 
 app = Eve()
+cors = CORS(app, resources={r'/api/v1/*': {"origins": "*"}})
 
-
-@app.route("/images/random")
+@app.route("/api/v1/images/random")
 def get_random_image():
     return jsonify({'url': 'http://www.imgion.com/images/01/beautiful-village-home.jpg'})
 
