aef2573bdeb249787c7e41ce04a44aca6e171cba
Omer Toledano <omertoledano1@gmail.com>
2014-10-15 15:41:24 +0300

    Added post request for liking

--- run.py
+++ run.py
@@ -1,13 +1,23 @@
+import json
 from eve import Eve
-from flask import jsonify
+from flask import jsonify, request
 from flask_cors import CORS
 
 app = Eve()
+app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r'/api/v1/*': {"origins": "*"}})
+
+
 
 @app.route("/api/v1/images/random")
 def get_random_image():
-    return jsonify({'url': 'http://www.imgion.com/images/01/beautiful-village-home.jpg'})
+    return jsonify({'url': 'http://www.imgion.com/images/01/beautiful-village-home.jpg', 'id': 1})
+
+
+@app.route("/api/v1/image/<int:image_id>/like", methods=['POST'])
+def save_op(image_id):
+    print image_id, json.loads(request.data)['op']
+    return jsonify({})
 
 
 if __name__ == '__main__':
